68e7697a6e384f62e388bb912399614e2c26f2f4
Azam <abdurazakovazam01@gmail.com>
2023-01-10 17:18:46 +0500

    AddComments, form

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostView.as_view(), name='posts'),
+    path('', views.PostView.as_view()),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post'),
+    path('add_comments/<int:pk>/', views.AddComments.as_view(), name='add_comments'),
 ]
